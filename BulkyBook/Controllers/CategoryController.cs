7f627f3c35092cc0d7da924e41112f80a9d4f1fd
stjepansuhic@yahoo.com <stjepansuhic@yahoo.com>
2024-03-10 11:09:32 +0100

    Add project files.

---
+++ BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBook.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly BulkyDbContext _db;
+
+        public CategoryController(BulkyDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
